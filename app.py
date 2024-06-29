a0f03493a5918b616b9ed652b177dd25e3bf9f44
KiPTM <133209150+KiPTM@users.noreply.github.com>
2024-06-29 17:13:43 +0000

    update app.py

--- app.py
+++ app.py
@@ -1,38 +1,43 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
-JOBS = [{
-    'id': 1,
-    'title': 'Financial Analyst',
-    'location': 'Nairobi, Kenya',
-    'salary': 'KES. 9000,000'
-}, {
-    'id': 2,
-    'title': 'Data Scientist',
-    'location': 'Cairo, Egypt',
-    'salary': 'USD. 10,000'
-}, {
-    'id': 3,
-    'title': 'Commercial Analyst',
-    'location': 'New York, USA',
-    'salary': 'USD. 100,000'
-}, {
-    'id': 4,
-    'title': 'Backend Engineer',
-    'location': 'Cape Town, South Africa',
-    'salary': 'Rand. 1,000,000'
-}]
-
 
 @app.route("/")
-def hello_world():
-  return render_template('home.html', jobs=JOBS, company_name='Kratos')
-
+def hello_kratos():
+  jobs = load_jobs_from_db()
+  return render_template('home.html', 
+                         jobs=jobs)
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+
+  if not job:
+    return "Not Found", 404
+
+  return render_template('jobpage.html', 
+                         job=job)
+
+@app.route("/api/job/<id>")
+def show_job_json(id):
+  job = load_job_from_db(id)
+  return jsonify(job)
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  add_application_to_db(id, data)
+  return render_template('application_submitted.html', 
+                         application=data,
+                         job=job)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
